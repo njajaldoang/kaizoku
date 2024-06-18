67796f8510dba9b66cc42d349809a2c2b2911301
njajaldoang <158686130+njajaldoang@users.noreply.github.com>
2024-06-18 22:28:49 +0700

    Update sysvshm.sh

--- sysvshm.sh
+++ sysvshm.sh
@@ -1,9 +1,39 @@
 start_dir="/storage/emulated/0/"
 home_dir="/data/data/com.termux/files/home/"
 TMPDIR="/data/data/com.termux/files/usr/tmp/"
+dir1=/data/data/com.termux/files/usr/bin
+dir2=/data/data/com.termux/files/home
+SP=/storage/emulated/0/1dra
 urut=xroot
 urutz=zroot
 uruts=sbin
 protz=dev
 prots=sys
 protx=substr
+function cling() {
+clear
+}
+
+function print() {
+echo "$1"
+}
+
+function input() {
+read -p "$1" $2
+}
+
+function turu() {
+sleep $1 
+}
+
+function install_paket() {
+pkg update -y ; pkg upgrade -y ; pkg install libpolly -y ; pkg install shc -y ; pkg install binutils -y
+}
+
+function end() {
+exit 0
+}
+
+function abort() {
+exit 1
+} 
